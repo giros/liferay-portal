cbce7172693452deac540b1a5446a7cc97e80360
Gergely Mathe <gergely.mathe@liferay.com>
2014-08-21 14:46:02 +0200

    LPS-49365 Make staging and export/import background tasks non-serial and apply group level locking where necessary

--- portal-impl/src/com/liferay/portal/lar/backgroundtask/BaseStagingBackgroundTaskExecutor.java
+++ portal-impl/src/com/liferay/portal/lar/backgroundtask/BaseStagingBackgroundTaskExecutor.java
@@ -43,7 +43,7 @@ public abstract class BaseStagingBackgroundTaskExecutor
 		setBackgroundTaskStatusMessageTranslator(
 			new DefaultExportImportBackgroundTaskStatusMessageTranslator());
 
-		setSerial(true);
+		setSerial(false);
 	}
 
 	@Override
