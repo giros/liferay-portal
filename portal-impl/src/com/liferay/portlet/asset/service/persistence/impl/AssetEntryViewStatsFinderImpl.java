b6c48279d0a8d1017f0035d539760a372adfb923
giros <gergely.mathe@liferay.com>
2017-02-27 22:03:50 +0100

    Exercise #2 - Implement custom finder methods

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryViewStatsFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryViewStatsFinderImpl.java
@@ -15,12 +15,96 @@
 package com.liferay.portlet.asset.service.persistence.impl;
 
 import com.liferay.asset.kernel.model.AssetEntryViewStats;
-import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.asset.kernel.service.persistence.AssetEntryViewStatsFinder;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.util.dao.orm.CustomSQLUtil;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Gergely Mathe
  */
 public class AssetEntryViewStatsFinderImpl
-	extends AssetEntryViewStatsFinderBaseImpl {
+	extends AssetEntryViewStatsFinderBaseImpl
+		implements AssetEntryViewStatsFinder {
+
+	public static final String COUNT_BY_C_C_GTD_LTD =
+		AssetEntryViewStatsFinder.class.getName() + ".countByC_C_LtD_GtD";
+
+	public static final String FIND_BY_C_C_GTD_LTD =
+		AssetEntryViewStatsFinder.class.getName() + ".findByC_C_LtD_GtD";
+
+	public long countByC_C_LtD_GtD(
+		long classNameId, long classPK, Date startDate, Date endDate) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(COUNT_BY_C_C_GTD_LTD);
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(classNameId);
+			qPos.add(classPK);
+			qPos.add(startDate);
+			qPos.add(endDate);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count;
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	public List<AssetEntryViewStats> findByC_C_LtD_GtD(
+		long classNameId, long classPK, Date startDate, Date endDate) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(FIND_BY_C_C_GTD_LTD);
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(classNameId);
+			qPos.add(classPK);
+			qPos.add(startDate);
+			qPos.add(endDate);
+
+			return q.list(true);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
 
 }
