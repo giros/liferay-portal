8356416707fce55b06c3f6a66c4948cbfaf9ed31
Gergely Mathe <gergely.mathe@liferay.com>
2017-11-29 09:33:34 +0100

    LPS-76046 testGetDataPortletPreferences()

--- portal-test-integration/src/com/liferay/portal/lar/test/BasePortletDataHandlerTestCase.java
+++ portal-test-integration/src/com/liferay/portal/lar/test/BasePortletDataHandlerTestCase.java
@@ -65,6 +65,13 @@ public abstract class BasePortletDataHandlerTestCase {
 	}
 
 	@Test
+	public void testGetDataPortletPreferences() {
+		Assert.assertArrayEquals(
+			getDataPortletPreferences(),
+			portletDataHandler.getDataPortletPreferences());
+	}
+
+	@Test
 	public void testIsDataPortalLevel() {
 		Assert.assertEquals(
 			isDataPortalLevel(), portletDataHandler.isDataPortalLevel());
@@ -172,6 +179,10 @@ public abstract class BasePortletDataHandlerTestCase {
 
 	protected DataLevel getDataLevel() {
 		return DataLevel.SITE;
+	}
+
+	protected String[] getDataPortletPreferences() {
+		return StringPool.EMPTY_ARRAY;
 	}
 
 	protected Date getEndDate() {
