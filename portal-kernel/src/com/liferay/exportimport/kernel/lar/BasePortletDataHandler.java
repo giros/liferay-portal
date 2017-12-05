2435cfe2213b3ac45483ba20a01e380e66dc3750
Gergely Mathe <gergely.mathe@liferay.com>
2017-12-05 11:15:40 +0100

    LPS-76046 Un-deprecate get/setPortletId

--- portal-kernel/src/com/liferay/exportimport/kernel/lar/BasePortletDataHandler.java
+++ portal-kernel/src/com/liferay/exportimport/kernel/lar/BasePortletDataHandler.java
@@ -524,10 +524,6 @@ public abstract class BasePortletDataHandler implements PortletDataHandler {
 		}
 	}
 
-	/**
-	 * @deprecated As of 7.0.0, with no direct replacement
-	 */
-	@Deprecated
 	@Override
 	public void setPortletId(String portletId) {
 		_portletId = portletId;
