e14c4b7f901656afef56030bfb61e7ba2f129ea4
Gergely Mathe <gergely.mathe@liferay.com>
2018-07-19 10:55:23 +0200

    LPS-79312 Add/extend verify processes to update portletIds if necessary

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/verify/BlogsServiceVerifyProcess.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/verify/BlogsServiceVerifyProcess.java
@@ -12,92 +12,41 @@
  * details.
  */
 
-package com.liferay.bookmarks.internal.verify;
+package com.liferay.blogs.internal.verify;
 
-import com.liferay.bookmarks.constants.BookmarksPortletKeys;
-import com.liferay.bookmarks.model.BookmarksFolder;
-import com.liferay.bookmarks.service.BookmarksFolderLocalService;
+import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.exportimport.kernel.staging.Staging;
-import com.liferay.portal.instances.service.PortalInstancesLocalService;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.util.LoggingTimer;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.verify.VerifyProcess;
-
-import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Raymond Augé
- * @author Alexander Chow
+ * @author Gergely Mathe
  */
 @Component(
 	immediate = true,
-	property = "verify.process.name=com.liferay.bookmarks.service",
+	property = "verify.process.name=com.liferay.blogs.service",
 	service = VerifyProcess.class
 )
-public class BookmarksServiceVerifyProcess extends VerifyProcess {
+public class BlogsServiceVerifyProcess extends VerifyProcess {
 
 	@Override
 	protected void doVerify() throws Exception {
-		updateFolderAssets();
 		updateStagedPortletNames();
-		verifyTree();
-	}
-
-	@Reference(unbind = "-")
-	protected void setBookmarksFolderLocalService(
-		BookmarksFolderLocalService bookmarksFolderLocalService) {
-
-		_bookmarksFolderLocalService = bookmarksFolderLocalService;
 	}
 
 	@Reference(unbind = "-")
 	protected void setGroupLocalService(GroupLocalService groupLocalService) {
 		_groupLocalService = groupLocalService;
-	}
-
-	protected void updateFolderAssets() throws Exception {
-		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			List<BookmarksFolder> folders =
-				_bookmarksFolderLocalService.getNoAssetFolders();
-
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					"Processing " + folders.size() + " folders with no asset");
-			}
-
-			for (BookmarksFolder folder : folders) {
-				try {
-					_bookmarksFolderLocalService.updateAsset(
-						folder.getUserId(), folder, null, null, null, null);
-				}
-				catch (Exception e) {
-					if (_log.isWarnEnabled()) {
-						_log.warn(
-							StringBundler.concat(
-								"Unable to update asset for folder ",
-								String.valueOf(folder.getFolderId()), ": ",
-								e.getMessage()));
-					}
-				}
-			}
-
-			if (_log.isDebugEnabled()) {
-				_log.debug("Assets verified for folders");
-			}
-		}
 	}
 
 	protected void updateStagedPortletNames() throws PortalException {
@@ -117,7 +66,7 @@ public class BookmarksServiceVerifyProcess extends VerifyProcess {
 					group.getTypeSettingsProperties();
 
 				String propertyKey = _staging.getStagedPortletId(
-					BookmarksPortletKeys.BOOKMARKS);
+					BlogsPortletKeys.BLOGS);
 
 				String propertyValue = typeSettingsProperties.getProperty(
 					propertyKey);
@@ -126,7 +75,7 @@ public class BookmarksServiceVerifyProcess extends VerifyProcess {
 					typeSettingsProperties.remove(propertyKey);
 
 					propertyKey = _staging.getStagedPortletId(
-						BookmarksPortletKeys.BOOKMARKS_ADMIN);
+						BlogsPortletKeys.BLOGS_ADMIN);
 
 					typeSettingsProperties.put(propertyKey, propertyValue);
 				}
@@ -135,25 +84,7 @@ public class BookmarksServiceVerifyProcess extends VerifyProcess {
 		groupActionableDynamicQuery.performActions();
 	}
 
-	protected void verifyTree() throws Exception {
-		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			long[] companyIds =
-				_portalInstancesLocalService.getCompanyIdsBySQL();
-
-			for (long companyId : companyIds) {
-				_bookmarksFolderLocalService.rebuildTree(companyId);
-			}
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		BookmarksServiceVerifyProcess.class);
-
-	private BookmarksFolderLocalService _bookmarksFolderLocalService;
 	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private PortalInstancesLocalService _portalInstancesLocalService;
 
 	@Reference
 	private Staging _staging;
