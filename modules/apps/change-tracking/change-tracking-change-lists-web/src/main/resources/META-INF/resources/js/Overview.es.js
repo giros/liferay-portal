98f4c4e878cb8deeba89fa2e0be3470174d8db37
Máté Thurzó <mate.thurzo@liferay.com>
2019-02-01 14:01:45 +0100

    LPS-89889 SF and refactoring

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
@@ -3,9 +3,6 @@ import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
 import {Config} from 'metal-state';
 import {openToast} from 'frontend-js-web/liferay/toast/commands/OpenToast.es';
 
-
-import CTCollectionModel from './CTCollectionModel.es';
-
 import templates from './Overview.soy';
 
 /**
@@ -15,7 +12,7 @@ import templates from './Overview.soy';
 class Overview extends PortletBase {
 
 	created() {
-	let urls = [this.urlActiveCollection, this.urlProductionInformation];
+		let urls = [this.urlActiveCollection, this.urlProductionInformation];
 
 		let headers = new Headers();
 		headers.append('Content-Type', 'application/json');
@@ -26,13 +23,13 @@ class Overview extends PortletBase {
 			method: 'GET'
 		};
 
-		this._fetchAll(urls, init)
-			.then(result => {
-				this.initialFetch = true;
-
-				//urlActiveCollection
-
-				let activeCollection = CTCollectionModel.build(result[0]);
+		this._fetchAll(
+			urls,
+			init
+		).then(
+			result => {
+				let activeCollection = result[0];
+				let productionInformation = result[1];
 
 				this.changes = {
 					added: activeCollection.additionCount,
@@ -40,79 +37,63 @@ class Overview extends PortletBase {
 					modified: activeCollection.modificationCount
 				};
 				this.descriptionActiveChangeList = activeCollection.description;
+				this.descriptionProductionInformation = productionInformation.ctcollection.description;
 				this.headerDropDownMenu = [
-					{label: 'Change List 01',
-link: 'link01'},
-					{label: 'Change List 02',
-link: 'link02'},
-					{label: 'Change List 03',
-link: 'link03'}
+					{
+						label: 'Change List 01',
+						link: 'link01'
+					},
+					{
+						label: 'Change List 02',
+						link: 'link02'
+					},
+					{
+						label: 'Change List 03',
+						link: 'link03'
+					}
 				];
 				this.headerTitleActiveChangeList = activeCollection.name;
+				this.headerTitleProductionInformation = productionInformation.ctcollection.name;
+				this.initialFetch = true;
 
-				//urlProductionCollection
+				let publishDate = new Date(productionInformation.date);
+				let publishDateFormatOptions = {
+					day: 'numeric',
+					hour: 'numeric',
+					minute: 'numeric',
+					month: 'numeric',
+					year: 'numeric'
+				};
 
-				let productionCollection = CTCollectionModel.build(result[1]);
-
-				this.descriptionProductionActiveChangeList = productionCollection.description;
-				this.headerTitleProductionChangeList = productionCollection.name;
 				this.publishedBy = {
-					dateTime: productionCollection.statusDate,
-					userIconUrl: '',
-					userMonogram: 'TT',
-					userName: productionCollection.statusByUserName
+					dateTime: new Intl.DateTimeFormat(Liferay.ThemeDisplay.getBCP47LanguageId(), publishDateFormatOptions).format(publishDate),
+					userInitials: productionInformation.userInitials,
+					userName: productionInformation.userName,
+					userPortraitURL: productionInformation.userPortraitURL
 				};
 			});
 	}
 
 	_fetchAll(urls, init) {
 		return Promise.all(
-			urls.map(url => fetch(url, init)
-				.then(r => r.json())
-				.then(data => data[0])
-				.catch(error => {
-					const message = Liferay.Language.get('error');
+			urls.map(
+				url => fetch(url, init)
+					.then(r => r.json())
+					.then(data => data[0])
+					.catch(
+						error => {
+							const message = Liferay.Language.get('error');
 
-					openToast(
-						{
-							message,
-							title: Liferay.Language.get('error'),
-							type: 'danger'
-						}
-					);
-				})
+							openToast(
+								{
+									message,
+									title: Liferay.Language.get('error'),
+									type: 'danger'
+								}
+							);
+						})
 			)
 		);
-	}
-
-	_getDataRequest(url, callback) {
-		let headers = new Headers();
-		headers.append('Content-Type', 'application/json');
-
-		const request = {
-			credentials: 'include',
-			headers,
-			method: 'GET'
-		};
-
-		fetch(url, request)
-			.then(response => response.json())
-			.then(response => callback(response[0]))
-			.catch(
-				(error) => {
-					const message = typeof error === 'string' ?
-						error :
-						Liferay.Language.get('an-error-occured-during-getting-the-production-information');
-
-					openToast(
-						{
-							message,
-							title: Liferay.Language.get('error'),
-							type: 'danger'
-						}
-					);
-				}
-			);
 	}
 
 }
@@ -155,11 +136,11 @@ Overview.STATE = {
 	 * Card description
 	 * @default
 	 * @instance
-	 * @memberOf ChangeList
+	 * @memberOf Overview
 	 * @review
 	 * @type {String}
 	 */
-	descriptionProductionChangeList: Config.string(),
+	descriptionProductionInformation: Config.string(),
 
 	/**
 	 * List of drop down menu items
@@ -192,11 +173,11 @@ Overview.STATE = {
 	 * Card header title
 	 * @default
 	 * @instance
-	 * @memberOf ChangeList
+	 * @memberOf Overview
 	 * @review
 	 * @type {String}
 	 */
-	headerTitleProductionChangeList: Config.string(),
+	headerTitleProductionInformation: Config.string(),
 
 	/**
 	 * If true, an initial fetch has already happened
@@ -230,7 +211,7 @@ Overview.STATE = {
 	 * Api url
 	 * @default
 	 * @instance
-	 * @memberOf ChangeList
+	 * @memberOf Overview
 	 * @review
 	 * @type {!String}
 	 */
