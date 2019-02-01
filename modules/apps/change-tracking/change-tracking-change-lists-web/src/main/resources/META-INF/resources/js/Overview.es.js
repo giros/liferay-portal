9936342252c75d21a700601e66f84381e861ce21
Máté Thurzó <mate.thurzo@liferay.com>
2019-02-01 14:45:40 +0100

    LPS-89889 SF and refactoring

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
@@ -26,52 +26,91 @@ class Overview extends PortletBase {
 		this._fetchAll(
 			urls,
 			init
-		).then(
-			result => {
-				let activeCollection = result[0];
-				let productionInformation = result[1];
+		)
+			.then(result => this._populateFields(result))
+			.catch(
+				error => {
+					const message = typeof error === 'string' ?
+						error :
+						Liferay.Language.get('an-error-occured-during-parsing-data');
 
-				this.changes = {
-					added: activeCollection.additionCount,
-					deleted: activeCollection.deletionCount,
-					modified: activeCollection.modificationCount
-				};
-				this.descriptionActiveChangeList = activeCollection.description;
-				this.descriptionProductionInformation = productionInformation.ctcollection.description;
-				this.headerDropDownMenu = [
-					{
-						label: 'Change List 01',
-						link: 'link01'
-					},
-					{
-						label: 'Change List 02',
-						link: 'link02'
-					},
-					{
-						label: 'Change List 03',
-						link: 'link03'
-					}
-				];
-				this.headerTitleActiveChangeList = activeCollection.name;
-				this.headerTitleProductionInformation = productionInformation.ctcollection.name;
-				this.initialFetch = true;
+					openToast(
+						{
+							message,
+							title: Liferay.Language.get('error'),
+							type: 'danger'
+						}
+					);
+				}
+			);
+	}
 
-				let publishDate = new Date(productionInformation.date);
-				let publishDateFormatOptions = {
-					day: 'numeric',
-					hour: 'numeric',
-					minute: 'numeric',
-					month: 'numeric',
-					year: 'numeric'
-				};
+	_populateFields(requestResult) {
+		let activeCollection = requestResult[0];
+		let productionInformation = requestResult[1];
 
-				this.publishedBy = {
-					dateTime: new Intl.DateTimeFormat(Liferay.ThemeDisplay.getBCP47LanguageId(), publishDateFormatOptions).format(publishDate),
-					userInitials: productionInformation.userInitials,
-					userName: productionInformation.userName,
-					userPortraitURL: productionInformation.userPortraitURL
-				};
-			});
+		// Changes
+
+		this.changes = {
+			added: activeCollection.additionCount,
+			deleted: activeCollection.deletionCount,
+			modified: activeCollection.modificationCount
+		};
+
+		// Active Change List Description
+
+		this.descriptionActiveChangeList = activeCollection.description;
+
+		// Production Information Description
+
+		this.descriptionProductionInformation = productionInformation.ctcollection.description;
+
+		// Change Lists dropdown Menu
+
+		this.changeListsDropdownMenu = [
+			{
+				label: 'Change List 01',
+				link: 'link01'
+			},
+			{
+				label: 'Change List 02',
+				link: 'link02'
+			},
+			{
+				label: 'Change List 03',
+				link: 'link03'
+			}
+		];
+
+		// Active Change List Header Title
+
+		this.headerTitleActiveChangeList = activeCollection.name;
+
+		// Production Information Header Title
+
+		this.headerTitleProductionInformation = productionInformation.ctcollection.name;
+
+		// Initial Fetch
+
+		this.initialFetch = true;
+
+		// Production Information Published By
+
+		let publishDate = new Date(productionInformation.date);
+		let publishDateFormatOptions = {
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			month: 'numeric',
+			year: 'numeric'
+		};
+
+		this.publishedBy = {
+			dateTime: new Intl.DateTimeFormat(Liferay.ThemeDisplay.getBCP47LanguageId(), publishDateFormatOptions).format(publishDate),
+			userInitials: productionInformation.userInitials,
+			userName: productionInformation.userName,
+			userPortraitURL: productionInformation.userPortraitURL
+		};
 	}
 
 	_fetchAll(urls, init) {
@@ -82,7 +121,9 @@ class Overview extends PortletBase {
 					.then(data => data[0])
 					.catch(
 						error => {
-							const message = Liferay.Language.get('error');
+							const message = typeof error === 'string' ?
+								error :
+								Lang.sub(Liferay.Language.get('an-error-occured-during-getting-data-from-x'), [url]);
 
 							openToast(
 								{
@@ -91,7 +132,8 @@ class Overview extends PortletBase {
 									type: 'danger'
 								}
 							);
-						})
+						}
+					)
 			)
 		);
 	}
@@ -150,7 +192,7 @@ Overview.STATE = {
 	 * @review
 	 * @type {Array}
 	 */
-	headerDropDownMenu: Config.arrayOf(
+	changeListsDropdownMenu: Config.arrayOf(
 		Config.shapeOf(
 			{
 				label: Config.string(),
