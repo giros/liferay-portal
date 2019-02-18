e48e7c787f3c7b77ae803b745f2f4f134b8a7b35
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-02-18 17:26:56 +0100

    WIP

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTEntryBagImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTEntryBagImpl.java
@@ -16,24 +16,23 @@ package com.liferay.change.tracking.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.change.tracking.service.CTEntryLocalServiceUtil;
+
+import java.util.List;
+
 /**
- * The extended model implementation for the CTEntryBag service. Represents a row in the &quot;CTEntryBag&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.change.tracking.model.CTEntryBag<code> interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Daniel Kocsis
  */
 @ProviderType
 public class CTEntryBagImpl extends CTEntryBagBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a ct entry bag model instance should use the {@link com.liferay.change.tracking.model.CTEntryBag} interface instead.
-	 */
 	public CTEntryBagImpl() {
+	}
+
+	public List<CTEntry> getRelatedCTEntries() {
+		return CTEntryLocalServiceUtil.getCTEntryBagCTEntries(
+			getCtEntryBagId());
 	}
 
 }
